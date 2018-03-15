892b8a04db66a6e2cba98cb4a7777dea83a8a96d
mac126 <mac126@mac126deMacBook-Air.local>
2018-03-15 18:44:26 +0800

    注释

--- Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/ThresholdPanGesture.swift
+++ Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/ThresholdPanGesture.swift
@@ -36,11 +36,13 @@ class ThresholdPanGesture: UIPanGestureRecognizer {
     }
     
     /// Returns the threshold value that should be used dependent on the number of touches.
+    /// 根据触摸手指个数返回应使用的阈值。
     private static func threshold(forTouchCount count: Int) -> CGFloat {
         switch count {
         case 1: return 30
             
         // Use a higher threshold for gestures using more than 1 finger. This gives other gestures priority.
+        // 多于一个手指的手势使用较高的阈值。 这使其他手势优先。
         default: return 60
         }
     }
@@ -52,12 +54,15 @@ class ThresholdPanGesture: UIPanGestureRecognizer {
         let translationMagnitude = translation(in: view).length
         
         // Adjust the threshold based on the number of touches being used.
+        // 根据触摸手指个数调整阈值
         let threshold = ThresholdPanGesture.threshold(forTouchCount: touches.count)
         
         if !isThresholdExceeded && translationMagnitude > threshold {
             isThresholdExceeded = true
             
             // Set the overall translation to zero as the gesture should now begin.
+            // translation 在指定视图的坐标系中进行翻译
+            // 手势现在开始时，将整体翻译设置为零
             setTranslation(.zero, in: view)
         }
     }
