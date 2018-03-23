8dab600ba45c3291465a0cd716f0e0f5d6fc4864
mac126 <mac126@mac126deMacBook-Air.local>
2018-03-23 11:00:11 +0800

    添加拍照录制功能

--- Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/ViewController.swift
+++ Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/ViewController.swift
@@ -8,6 +8,7 @@ Main view controller for the AR experience.
 import ARKit
 import SceneKit
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
     
@@ -20,7 +21,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var blurView: UIVisualEffectView!
     
     @IBOutlet weak var spinner: UIActivityIndicatorView!
-
+    
+    /// 录制相关
+    var recorder:RecordAR?
+    let recordingQueue = DispatchQueue(label: "recordingThread", attributes: .concurrent)
+    @IBOutlet weak var squishButton: SquishButton!
+    @IBOutlet weak var segmentedControl: SegmentedControl!
+    
+    
     // MARK: - UI Elements
     
     var focusSquare = FocusSquare()
@@ -54,6 +62,8 @@ class ViewController: UIViewController {
     var session: ARSession {
         return sceneView.session
     }
+    
+    let configuration = ARWorldTrackingConfiguration()
     
     // MARK: - View Controller Life Cycle
     
@@ -90,6 +100,16 @@ class ViewController: UIViewController {
         // Set the delegate to ensure this gesture is only used when there are no virtual objects in the scene.
         tapGesture.delegate = self
         sceneView.addGestureRecognizer(tapGesture)
+        
+        // 录制设置
+        setupRecorder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Prepare the recorder with sessions configuration 录制
+        recorder?.prepare(configuration)
     }
 
 	override func viewDidAppear(_ animated: Bool) {
@@ -106,7 +126,142 @@ class ViewController: UIViewController {
 		super.viewWillDisappear(animated)
 
         session.pause()
+        
+        // 录制
+        if recorder?.status == .recording {
+            recorder?.stopAndExport()
+        }
+        recorder?.onlyRenderWhileRecording = true
+        recorder?.prepare(ARWorldTrackingConfiguration())
+        
+        // Switch off the orientation lock for UIViewControllers with AR Scenes
+        recorder?.rest()
 	}
+    
+    // MARK: - 录制按钮点击
+    @IBAction func recordVideo(_ sender: SquishButton) {
+        if sender.type == ButtonType.camera {
+            
+            let image = self.recorder?.photo()
+            self.recorder?.export(UIImage: image) { saved, status in
+                if saved {
+                    // Inform user photo has exported successfully
+                    self.exportMessage(success: saved, status: status)
+                }
+            }
+        } else if sender.type == ButtonType.video {
+            //Record
+            if recorder?.status == .readyToRecord {
+                sender.setTitle("停止", for: .normal)
+                
+                recordingQueue.async {
+                    self.recorder?.record()
+                }
+            }else if recorder?.status == .recording {
+                sender.setTitle("录制", for: .normal)
+                recorder?.stop() { path in
+                    self.recorder?.export(video: path) { saved, status in
+                        DispatchQueue.main.sync {
+                            self.exportMessage(success: saved, status: status)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: - SegmentedControl
+    
+    fileprivate func configureSegmentedControl() {
+        let titleStrings = ["可拍照", "录视频"]
+        let titles: [NSAttributedString] = {
+            let attributes: [NSAttributedStringKey: Any] = [.font: UIFont.systemFont(ofSize: 15), .foregroundColor: UIColor.lightGray]
+            var titles = [NSAttributedString]()
+            for titleString in titleStrings {
+                let title = NSAttributedString(string: titleString, attributes: attributes)
+                titles.append(title)
+            }
+            return titles
+        }()
+        let selectedTitles: [NSAttributedString] = {
+            let attributes: [NSAttributedStringKey: Any] = [.font: UIFont.systemFont(ofSize: 15), .foregroundColor: UIColor.white]
+            var selectedTitles = [NSAttributedString]()
+            for titleString in titleStrings {
+                let selectedTitle = NSAttributedString(string: titleString, attributes: attributes)
+                selectedTitles.append(selectedTitle)
+            }
+            return selectedTitles
+        }()
+        segmentedControl.setTitles(titles, selectedTitles: selectedTitles)
+        segmentedControl.delegate = self
+        segmentedControl.selectionBoxStyle = .none
+        segmentedControl.minimumSegmentWidth = 375.0 / 4.0
+        segmentedControl.selectionBoxColor = UIColor.clear
+        segmentedControl.selectionIndicatorStyle = .none
+        
+        // segmentedControl.selectionIndicatorColor = UIColor(white: 0.3, alpha: 1)
+    }
+    
+    // MARK: - Exported UIAlert present method
+    func exportMessage(success: Bool, status:PHAuthorizationStatus) {
+        if success {
+            let alert = UIAlertController(title: "导出", message: "导出相册成功", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if status == .denied || status == .restricted || status == .notDetermined {
+            let errorView = UIAlertController(title: "😅", message: "相册权限", preferredStyle: .alert)
+            let settingsBtn = UIAlertAction(title: "OpenSettings", style: .cancel) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+                    return
+                }
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                        })
+                    } else {
+                        UIApplication.shared.openURL(URL(string:UIApplicationOpenSettingsURLString)!)
+                    }
+                }
+            }
+            errorView.addAction(UIAlertAction(title: "Later", style: UIAlertActionStyle.default, handler: {
+                (UIAlertAction)in
+            }))
+            errorView.addAction(settingsBtn)
+            self.present(errorView, animated: true, completion: nil)
+        }else{
+            let alert = UIAlertController(title: "Exporting Failed", message: "There was an error while exporting your media file.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    /// MARK: - ARVideoKit
+    func setupRecorder() {
+        // Initialize ARVideoKit recorder
+        recorder = RecordAR(ARSceneKit: sceneView)
+        
+        /*----👇---- ARVideoKit Configuration ----👇----*/
+        
+        // Set the recorder's delegate
+        recorder?.delegate = self
+        
+        // Set the renderer's delegate
+        recorder?.renderAR = self
+        
+        // Configure the renderer to perform additional image & video processing 👁
+        recorder?.onlyRenderWhileRecording = false
+        
+        // Configure ARKit content mode. Default is .auto
+        recorder?.contentMode = .aspectFill
+        
+        // Set the UIViewController orientations
+        recorder?.inputViewOrientations = [.landscapeLeft, .landscapeRight, .portrait]
+        // Configure RecordAR to store media files in local app directory
+        recorder?.deleteCacheWhenExported = false
+        
+        // configureNavigationBelowSegmentedControl()
+        configureSegmentedControl()
+    }
 
     // MARK: - Scene content setup
 
@@ -129,7 +284,7 @@ class ViewController: UIViewController {
     
     /// Creates a new AR configuration to run on the `session`.
 	func resetTracking() {
-        let configuration = ARWorldTrackingConfiguration()
+        
         configuration.planeDetection = .horizontal
 		session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
 
@@ -191,4 +346,72 @@ class ViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
 
+}
+
+//MARK: - ARVideoKit Delegate Methods
+extension ViewController: RecordARDelegate, RenderARDelegate {
+    func frame(didRender buffer: CVPixelBuffer, with time: CMTime, using rawBuffer: CVPixelBuffer) {
+        // Do some image/video processing.
+    }
+    
+    func recorder(didEndRecording path: URL, with noError: Bool) {
+        if noError {
+            // Do something with the video path.
+        }
+    }
+    
+    func recorder(didFailRecording error: Error?, and status: String) {
+        // Inform user an error occurred while recording.
+    }
+    
+    func recorder(willEnterBackground status: RecordARStatus) {
+        // Use this method to pause or stop video recording. Check [applicationWillResignActive(_:)](https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1622950-applicationwillresignactive) for more information.
+        if status == .recording {
+            recorder?.stopAndExport()
+        }
+    }
+}
+
+extension ViewController: SegmentedControlDelegate {
+    func segmentedControl(_ segmentedControl: SegmentedControl, didSelectIndex selectedIndex: Int) {
+        print("Did select index \(selectedIndex)")
+        switch segmentedControl.style {
+        case .text:
+            print("The title is “\(segmentedControl.titles[selectedIndex].string)”\n")
+        case .image:
+            print("The image is “\(segmentedControl.images[selectedIndex])”\n")
+        }
+        
+        switch selectedIndex {
+        case 0: //
+            squishButton.type = ButtonType.camera
+        case 1:
+            squishButton.type = ButtonType.video
+            
+        default:
+            print("hhhhh")
+        }
+    }
+    
+    func segmentedControl(_ segmentedControl: SegmentedControl, didLongPressIndex longPressIndex: Int) {
+        print("Did long press index \(longPressIndex)")
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            let viewController = UIViewController()
+            viewController.modalPresentationStyle = .popover
+            viewController.preferredContentSize = CGSize(width: 200, height: 300)
+            if let popoverController = viewController.popoverPresentationController {
+                popoverController.sourceView = view
+                let yOffset: CGFloat = 10
+                popoverController.sourceRect = view.convert(CGRect(origin: CGPoint(x: 70 * CGFloat(longPressIndex), y: yOffset), size: CGSize(width: 70, height: 30)), from: navigationItem.titleView)
+                popoverController.permittedArrowDirections = .any
+                present(viewController, animated: true, completion: nil)
+            }
+        } else {
+            let message = segmentedControl.style == .text ? "Long press title “\(segmentedControl.titles[longPressIndex].string)”" : "Long press image “\(segmentedControl.images[longPressIndex])”"
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(cancelAction)
+            present(alert, animated: true, completion: nil)
+        }
+    }
 }
