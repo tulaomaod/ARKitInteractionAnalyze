0c8415f80cca876916d147447fb482cc7efa03b6
mac126 <mac126@mac126deMacBook-Air.local>
2018-03-23 10:20:32 +0800

    添加ARVideoKit，SegmentedControl，SquishButton

---
+++ Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/SegmentedControl/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SegmentedControl
+//
+//  Created by Xin Hong on 15/12/29.
+//  Copyright © 2015年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+internal struct SelectionIndicator {
+    static let defaultHeight: CGFloat = 5
+}
+
+internal struct Constant {
+    static let defaultSelectionHorizontalPadding: CGFloat = 15
+}
---
+++ Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/SegmentedControl/Style.swift
@@ -0,0 +1,30 @@
+//
+//  Style.swift
+//  SegmentedControl
+//
+//  Created by Xin Hong on 15/12/30.
+//  Copyright © 2015年 Teambition. All rights reserved.
+//
+
+import Foundation
+
+public enum SegmentedControlLayoutPolicy {
+    case fixed
+    case dynamic
+}
+
+public enum SegmentedControlStyle {
+    case text
+    case image
+}
+
+public enum SegmentedControlSelectionIndicatorStyle {
+    case none
+    case top
+    case bottom
+}
+
+public enum SegmentedControlSelectionBoxStyle {
+    case none
+    case `default`
+}
